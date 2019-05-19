4c99bbf4cea6123768a9f09ae1966e2005137a98
adargut <adar276@gmail.com>
2019-05-19 20:11:55 +0300

    Branch updating - more functions written

--- MainAux.c
+++ MainAux.c
@@ -1,11 +1,16 @@
-//
-// Created by User on 19/05/2019.
-//
-
 #include "MainAux.h"
 #include <Stdio.h>
 #include <stdlib.h>
 /* method used to print the current board */
+int i;
+int j;
+
 void printBoard(int board [9][9]) {
-    printf("----------------------------------\n");}
+    printf("----------------------------------\n");
+    for (i = 0; i < 9; i++) {
+        printf("|");
+        for (j = 0; j < 9; j++) {
+
+        }
+    }
 }
--- MainAux.h
+++ MainAux.h
@@ -3,10 +3,13 @@
 #ifndef SUDOKU_MAINAUX_H
 #define SUDOKU_MAINAUX_H
 
+/* function to print board in correct format */
 void printBoard(int board [9][9]);
 
+/* function to initialize the board */
 void initializeGame(int board[9][9]);
 
-void stamFunc();
+/* function to determine if board[x][y] is fixed */
+int isFixed(int row, int col, int board[9][9]);
 
 #endif //SUDOKU_MAINAUX_H
