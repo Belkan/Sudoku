ceecee10164668d9206fe1f2f8bb6a35511457b3
adargut <adar276@gmail.com>
2019-05-19 22:12:29 +0300

    Added printing method

--- MainAux.h
+++ MainAux.h
@@ -3,8 +3,17 @@
 #ifndef SUDOKU_MAINAUX_H
 #define SUDOKU_MAINAUX_H
 
+/* utility function to print board */
 void printBoard(int board [9][9]);
 
+/* function to initialize board */
 void initializeGame(int board[9][9]);
+
+/* function to check if cell is fixed */
+int isFixed(int row, int col, int board[9][9]);
+
+int transformRow(int row);
+
+int transformCol(int col);
 
 #endif //SUDOKU_MAINAUX_H
