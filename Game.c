f7494a87f158fdcca6ba7cfbd24cd6f7aa562e8f
Belkan <omriporat@gmail.com>
2019-05-20 18:57:54 +0300

    change enum and change "set" to see if game is over

--- Game.c
+++ Game.c
@@ -2,19 +2,24 @@
 #include "Game.h"
 
 /* tries to set value in (row, col) in board, and returns the status of the request */
-enum status set (GameState *gameState, int row, int col, int value) {
-    enum status Status;
+SET_STATUS set (GameState *gameState, int row, int col, int value) {
+    SET_STATUS Status;
     if (isFixed(row, col, gameState)) {
-        Status = Fixed;
+        Status = CELL_FIXED;
         return Status;
     }
     if (isLegalMove(gameState, row, col, value)) {
-        Status = Invalid;
+        Status = ILLEGAL_MOVE;
         return Status;
     }
 
     gameState->board[row][col] = value;
-    Status = Success;
+    if (fullBoard(gameState)){
+        Status = GAME_OVER;
+        return Status;
+    }
+
+    Status = SUCCESS;
     return Status;
 
 }
@@ -25,6 +30,7 @@ void validate (GameState *gameState){}
 void restart (GameState *gameState){}
 
 /* TODO: check if it's in the same box (currently only checks row and col */
+/* checks if this set is a legal set (assuming input is valid) */
 bool isLegalMove(GameState *gameState, int row, int col, int value){
     int i;
     for (i = 0; i < 9; i++){
@@ -32,4 +38,17 @@ bool isLegalMove(GameState *gameState, int row, int col, int value){
             return false;
         }
     }
+}
+
+/* check if the board is full */
+bool fullBoard(GameState *gameState){
+    int i, j;
+    for (i = 0; i < 9; i++){
+        for (j = 0; j < 9; j++){
+            if (gameState->board[i][j] == 0) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
--- Game.h
+++ Game.h
@@ -1,17 +1,28 @@
 #ifndef SUDOKU_GAME_H
 #define SUDOKU_GAME_H
+
 #include "MainAux.h"
 
-enum status {Success, Fixed, Invalid, GameOver};
+typedef enum set_status {
+    SUCCESS,
+    CELL_FIXED,
+    ILLEGAL_MOVE,
+    GAME_OVER
+} SET_STATUS;
 
-enum status set (GameState *gameState, int row, int col, int value);
+SET_STATUS set(GameState *gameState, int row, int col, int value);
 
-void hint (GameState *gameState, int row, int col);
+void hint(GameState *gameState, int row, int col);
 
-void validate (GameState *gameState);
+void validate(GameState *gameState);
 
-void restart (GameState *gameState);
+void restart(GameState *gameState);
 
+/* checks if this set is a legal set (assuming input is valid) */
 bool isLegalMove(GameState *gameState, int row, int col, int value);
+
+/* check if the board is full */
+bool fullBoard(GameState *gameState);
+
 
 #endif
