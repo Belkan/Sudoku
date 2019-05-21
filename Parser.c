cd4c22c94fb1d0dfddf066419ba865f4312a9983
adargut <adar276@gmail.com>
2019-05-22 00:40:32 +0300

    Added funcs to parser module

--- Parser.c
+++ Parser.c
@@ -2,12 +2,43 @@
 #include <stdlib.h>
 #include "Parser.h"
 #include <string.h>
+#include <ctype.h>
 #define MAX 1024
 
 /* checks if user input matches given regular expressions */
 bool matchesFormat(char* str, USER_CHOICE choice) {
-    /* TODO check formatting of user input */
-    return true;
+    bool format = false;
+
+    switch (choice) {
+        case SET:
+            if(str[0] == 's' && str[1] == 'e' && str[2] == 't' &&
+                isdigit(str[3]) && isdigit(str[4]) && isdigit(str[5])) {
+                format = true;
+                break;
+            }
+        case HINT:
+            if(str[0] == 'h' && str[1] == 'i' && str[2] == 'n' && str[3] == 't' &&
+               isdigit(str[4]) && isdigit(str[5])) {
+                format = true;
+                break;
+            }
+        case VALIDATE:
+            if (strcmp(str, "validate") == 0) {
+                format = true;
+                break;
+            }
+        case RESTART:
+            if(strcmp(str, "restart") == 0) {
+                format = true;
+                break;
+            }
+        case EXIT:
+            if(strcmp(str, "exit") == 0) {
+                format = true;
+                break;
+            }
+    }
+    return format;
 } /* EOF */
 
 /* scan user input and return it as String format */
