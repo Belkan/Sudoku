cf63412576d608a58929c12fe7179627f86ffc5a
adargut <adar276@gmail.com>
2019-05-23 04:40:00 +0300

    Added Main Aux UT + getRandom func

--- MainAuxUnitTest.c
+++ MainAuxUnitTest.c
@@ -5,14 +5,14 @@
 
 void verify_printBoard() {
     GameState *gameState = createGameState(SIZE);
-    printf("this should print the empty board:\n");
+    printf("This should print the empty board:\n");
 
     printBoard(gameState);
     set(gameState, 1, 1, 1);
     set(gameState, 5,4,18);
     set(gameState, 4,3,9);
-    printf("this should print a board with some fixed cells\n");
-    
+    printf("This should print a board with some fixed cells\n");
+
     printBoard(gameState);
     destroyGameState(gameState);
 }
