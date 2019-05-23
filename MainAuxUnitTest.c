a2c7f4c00eff4879b75625e037be20340ddd7b3f
adargut <adar276@gmail.com>
2019-05-23 04:37:37 +0300

    Added Main Aux UT + getRandom func

--- MainAuxUnitTest.c
+++ MainAuxUnitTest.c
@@ -6,11 +6,13 @@
 void verify_printBoard() {
     GameState *gameState = createGameState(SIZE);
     printf("this should print the empty board:\n");
+
     printBoard(gameState);
     set(gameState, 1, 1, 1);
     set(gameState, 5,4,18);
     set(gameState, 4,3,9);
     printf("this should print a board with some fixed cells\n");
+    
     printBoard(gameState);
     destroyGameState(gameState);
 }
@@ -21,14 +23,21 @@ void verify_getRandom() {
     upper = INT_MAX;
     res = getRandom(lower, upper);
     printf("This should print an integer in [4, infinity]: %d\n", res);
+
     lower = 5;
     upper = 17;
     res = getRandom(lower, upper);
     printf("This should print an integer in [5,17]: %d\n", res);
+
     lower = 250;
     upper = 2796;
     res = getRandom(lower, upper);
     printf("This should print an integer in [250, 2796]: %d\n", res);
+
+    lower = 2;
+    upper = 3;
+    res = getRandom(lower, upper);
+    printf("This should print an integer in [2, 3]: %d\n", res);
 
 }
 
