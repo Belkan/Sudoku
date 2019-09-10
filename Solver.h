78a9a3aeb7b81606dd8254b717e574d6871a684d
Belkan <omriporat@gmail.com>
2019-09-10 12:09:46 +0300

    Add basic gurobi template and fake interface

--- Solver.h
+++ Solver.h
@@ -4,17 +4,31 @@
 #include "Game.h"
 #include "SolverUtils.h"
 
-typedef enum solution_type {
-    DETERMINISTIC,
-    RANDOM
-} SOLUTION_TYPE ;
+typedef struct Gurobi {
+    double* solution;
+    int error;
+
+} Gurobi ;
+
+typedef enum LinearMethod {
+    ILP,
+    LP
+} LinearMethod;
 
 /* TODO compare this with Ellie & Nofar */
 /* Use exhaustive backtracking algorithm (using stack) to count the number of solutions for the board. */
 int solutionCounter(GameState *gameState);
 
+Gurobi* getSolution(GameState *gameState, LinearMethod linearMethod);
+
 /* TODO Gurobi needs to check if board is solvable */
 bool isSolvable(GameState *gameState);
+
+Gurobi* createGurobi(int size) {
+    Gurobi* gurobi = malloc(sizeof(Gurobi));
+    gurobi->solution = (double*) malloc(size*sizeof(double));
+    return gurobi;
+}
 
 #endif
 
